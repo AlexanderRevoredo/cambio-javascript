ee8d9f8931cc4957f6c937ae8c8bd12949dd2a95
Alexander Revoredo <alexanderrevoredo99@gmail.com>
2025-01-31 16:45:42 -0300

    deixando o visual mais bonito

--- app.js
+++ app.js
@@ -88,3 +88,6 @@ app.get('/converter', async (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
+
+// git branch -M main
+// git push -u origin main
